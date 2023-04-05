76ae594f48f7fb0ae0f3929a2feca9ffd6a56000
tsehaidesouza <tsehaidesouza@gmail.com>
2023-04-05 13:28:45 -0400

    scripts and moo

---
+++ Assets/Scripts/PlantDelay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantDelay : MonoBehaviour
+{
+    public MeshRenderer stage1;
+    public MeshRenderer stage2;
+    public MeshRenderer stage3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

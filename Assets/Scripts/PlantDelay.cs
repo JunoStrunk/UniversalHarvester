935869f171bce2d0fbb24e8075ac5d9411a61b62
tsehaidesouza <tsehaidesouza@gmail.com>
2023-04-10 12:12:44 -0400

    is things idk

--- Assets/Scripts/PlantDelay.cs
+++ Assets/Scripts/PlantDelay.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class PlantDelay : MonoBehaviour
 {
-    public MeshRenderer stage1;
-    public MeshRenderer stage2;
-    public MeshRenderer stage3;
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stage1.SetActive(true);
+        stage2.SetActive(false);
+        stage3.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         StartCoroutine(Delay());
+       
     }
 
     IEnumerator Delay()
     {
+        stage1.SetActive(true);
+        stage2.SetActive(false);
+        stage3.SetActive(false);
+        yield return new WaitForSeconds(5);
+        stage1.SetActive(false);
+        stage2.SetActive(true);
+        stage3.SetActive(false);
+        yield return new WaitForSeconds(5);
+        stage1.SetActive(false);
+        stage2.SetActive(false);
+        stage3.SetActive(true);
 
     }
+  
 }

1ba5d6caf9e88768f5ea2297a008f31e81fbaeb2
tsehaidesouza <tsehaidesouza@gmail.com>
2023-04-12 12:05:30 -0400

    crypto nonsense

---
+++ Packages/com.careboo.unity-algorand-sdk/Runtime/zxing.unity/Source/lib/common/BigInteger/Sign.cs
@@ -0,0 +1,9 @@
+﻿namespace BigIntegerLibrary
+{
+
+    /// <summary>
+    /// The number's sign, where Positive also stands for the number zero.
+    /// </summary>
+    internal enum Sign { Positive, Negative };
+
+}

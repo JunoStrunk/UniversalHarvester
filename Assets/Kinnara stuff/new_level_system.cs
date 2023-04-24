10ff4ade94fb0291a52724f65c964d7dcf327c8a
JunoStrunk <junostrunk@gmail.com>
2023-04-24 16:41:12 -0400

    Connect sign to level system

--- Assets/Kinnara stuff/new_level_system.cs
+++ Assets/Kinnara stuff/new_level_system.cs
@@ -5,41 +5,44 @@ using UnityEngine.UI;
 
 public class new_level_system : MonoBehaviour
 {
-    public Image xp_bar;
-    public float current_xp = 0;
-    public float required_xp = 100;
-    public float excess_xp = 0;
-    public int level = 0;
-    public Text level_name;
+	public Image xp_bar;
+	public float current_xp = 0;
+	public float required_xp = 100;
+	public float excess_xp = 0;
+	public int level = 0;
+	public Text level_name;
+
+	public FarmFactSign sign;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        xp_bar.fillAmount = current_xp / required_xp;
-        Debug.Log("Start, " + xp_bar.fillAmount);
-        level_name.text = "Level: " + (level + 1);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		xp_bar.fillAmount = current_xp / required_xp;
+		Debug.Log("Start, " + xp_bar.fillAmount);
+		level_name.text = "Level: " + (level + 1);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (current_xp >= required_xp)
-        {
-            level++;
-            excess_xp = current_xp - required_xp;
-            current_xp = excess_xp;
-            required_xp = required_xp + (50 * (level));
-            xp_bar.fillAmount = excess_xp/required_xp;
-            level_name.text = "Level: " + (level + 1);
-        }
-        if (CropTracker.instance.isHarvestedKin==true)
-        {
-            current_xp += CropTracker.instance.xp;
-            Debug.Log(CropTracker.instance.xp);
-            xp_bar.fillAmount = current_xp / required_xp;
-            Debug.Log("Harvested, " + xp_bar.fillAmount);
-            CropTracker.instance.isHarvestedKin = false;
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (current_xp >= required_xp) //level up
+		{
+			level++;
+			excess_xp = current_xp - required_xp;
+			current_xp = excess_xp;
+			required_xp = required_xp + (50 * (level));
+			xp_bar.fillAmount = excess_xp / required_xp;
+			level_name.text = "Level: " + (level + 1);
+			sign.GetFarmFact();
+		}
+		if (CropTracker.instance.isHarvestedKin == true)
+		{
+			current_xp += CropTracker.instance.xp;
+			Debug.Log(CropTracker.instance.xp);
+			xp_bar.fillAmount = current_xp / required_xp;
+			Debug.Log("Harvested, " + xp_bar.fillAmount);
+			CropTracker.instance.isHarvestedKin = false;
+		}
+	}
 }
